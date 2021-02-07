bde6fdcc4fb9695a842d8efb75184c7adbf75d71
Alexander Fefelov <alexanderfefelov@yandex.ru>
2021-02-07 09:25:18 +0300

    Refactoring

--- examples/python/json-get-plugins.py
+++ examples/python/json-get-plugins.py
@@ -9,14 +9,14 @@ import urllib.request
 config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
 config.read('bgbilling.conf')
 
-endpoint_url = config.get('http.json', 'url_endpoint_plugincfg')
+url = config.get('http.json', 'url_endpoint_plugincfg')
 request_template = config.get('http.json', 'request_template')
 
 method = 'getPlugins'
 parameters = '{}'
 
 request_json = request_template % (method, parameters)
-request = urllib.request.Request(endpoint_url)
+request = urllib.request.Request(url)
 request.add_header('Content-Type', 'application/json; charset=utf-8')
 response = urllib.request.urlopen(request, json.dumps(json.loads(request_json)).encode('utf-8'))
 response_body = response.read().decode('utf8')
--- examples/python/soap-get-plugins.py
+++ examples/python/soap-get-plugins.py
@@ -8,14 +8,14 @@ import urllib.request
 config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
 config.read('bgbilling.conf')
 
-endpoint_url = config.get('http.soap', 'url_endpoint_plugincfg')
+url = config.get('http.soap', 'url_endpoint_plugincfg')
 request_template = config.get('http.soap', 'request_template')
 namespace = config.get('http.soap', 'namespace_plugincfg')
 
 call = '<getPlugins/>'
 
 request_xml = request_template % (namespace, call)
-request = urllib.request.Request(endpoint_url)
+request = urllib.request.Request(url)
 request.add_header('Content-Type', 'text/xml; charset=utf-8')
 response = urllib.request.urlopen(request, request_xml.encode('utf-8'))
 response_body = response.read().decode('utf8')
